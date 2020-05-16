82bcc1df17d65ed1c025d01c6649585f57f659c6
Katsuken <ngu11f0046@gmail.com>
2020-05-17 01:05:10 +0900

    Udemy Budget Appの作成

---
+++ main.js
@@ -0,0 +1,85 @@
+
+// budgetController
+var budgetController = (function (){
+
+})();
+
+
+// UI Controller
+var UIController = (function (){
+
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // income or expense
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMStrings;
+    }
+
+
+  };
+
+})();
+
+
+// controller
+var controller = (function (budgetCtrl, UICtrl){
+
+  var setupEventListeners = function() {
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+  
+    document.addEventListener('keypress', function(event) {
+      if(event.keyCode === 13 ||event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = function() {
+     // 1. Get input value
+     var input = UICtrl.getInput();
+     console.log(input);
+
+    // 2. Add item to the budget controller 
+
+    // 3. Add item to the UI controoller
+
+    // 4. Calculate total budget 
+
+    // 5. Display the budget to UI
+
+  };
+
+  return {
+    init: function() {
+      console.log('Application has started.');
+      setupEventListeners();
+    }
+  };
+  
+})(budgetController, UIController);
+
+controller.init();
+
+
+
+
+
+
+
+
+
